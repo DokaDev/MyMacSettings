d829e369b594142364c7b320c111208674a33776
Awesome <awesome_devnet@outlook.com>
2024-05-25 20:21:38 +0900

    create first settings

---
+++ prepare.sh
@@ -0,0 +1,21 @@
+# all: prepared
+
+# prepared: 
+# 	brew install make
+# 	brew install wget
+# 	brew install curl
+# 	brew install git
+
+# convert to sh
+# 1. install home brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# 2. install essential tools
+brew install make
+brew install wget
+brew install curl
+brew install git
+
+# docs
+# how to use the script in mac os
+# ./prepare.sh
